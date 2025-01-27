dc8d6b73536f2c9f81a74c346f0ddc36e4694826
aliciariveraa <aliciarivera@usf.edu>
2025-01-26 20:22:32 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,8 @@ let products = ["Apple", "Banana", "Carrot", "Donut", "Eggplant"];
 products.push("Fish");
 products.pop();
 console.log(products);  // Output: ["Apple", "Banana", "Carrot", "Donut"]
+let scores = [85, 92, 78, 88, 94];
+scores[1] = 95;
+average: 88 
+console.log("Updated scores array:", scores);
+console.log("Average score:", average);
