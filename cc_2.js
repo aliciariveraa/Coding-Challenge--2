e528215652b05ba86d5a06a4f9400a9fc6b9f800
aliciariveraa <aliciarivera@usf.edu>
2025-01-26 20:10:57 -0500

    "Task 1 - Store Inventory."

---
+++ cc_2.js
@@ -0,0 +1,4 @@
+let products = ["Apple", "Banana", "Carrot", "Donut", "Eggplant"];
+products.push("Fish");
+products.pop();
+console.log(products);  // Output: ["Apple", "Banana", "Carrot", "Donut"]
